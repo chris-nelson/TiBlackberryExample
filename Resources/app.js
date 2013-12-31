a0f8b41f083d99740d62653ed47f365c83c047c5
Chris Nelson <chrisnelson@hackintosh.gateway.2wire.net>
2013-12-31 01:22:10 -0500

    removed bloat

--- Resources/app.js
+++ Resources/app.js
@@ -1,13 +1,13 @@
 // This is a single context application with mutliple windows in a stack
 (function() {
-  //determine platform and form factor and render approproate components
+	
   var osname = Ti.Platform.osname,
     version = Ti.Platform.version,
     height = Ti.Platform.displayCaps.platformHeight,
     width = Ti.Platform.displayCaps.platformWidth;
 
 
-   var Window = require('ui/handheld/ApplicationWindow');
+   var Window = require('ui/ApplicationWindow');
    new Window().open();
    
 })();
